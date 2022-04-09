049ac6ae496de320dfad76415e868bddc9f106ab
sinbum Choi <sinbum@kakao.com>
2022-04-09 19:07:59 +0900

    테이블데이터 가져오기

--- crolling.py
+++ crolling.py
@@ -1,9 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html,'html.parser')
 
-nameList = bs.findAll('span',{'class':'green'})
-for name in nameList:
-    print(name.get_text())
+# nameList = bs.findAll('span',{'class':'green'})
+# for name in nameList:
+#     print(name.get_text())
+
+
+# 테이블 데이터 가져오기
+# for child in bs.find('table',{'id' : 'giftList'}).children:
+#     print(child)
