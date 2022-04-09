436c1297c36671c3b0b8deea2f8a3248cee742bb
sinbum Choi <sinbum@kakao.com>
2022-04-09 19:50:08 +0900

    정규식을 사용한 예제

--- crolling.py
+++ crolling.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html,'html.parser')
+import re
 
 # nameList = bs.findAll('span',{'class':'green'})
 # for name in nameList:
@@ -12,3 +13,12 @@ bs = BeautifulSoup(html,'html.parser')
 # 테이블 데이터 가져오기
 # for child in bs.find('table',{'id' : 'giftList'}).children:
 #     print(child)
+
+# for sibling in bs.find('table', {'id':'giftList'}).tr.next_siblings:
+#     print(sibling)
+
+# print(bs.find('img',{'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+
+images = bs.find_all('img', {'src':re.compile('\.\.\/img\/gifts/img.*\.jpg')})
+for image in images: 
+    print(image['src'])
