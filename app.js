2ccb1d6c8121d39b4d009f474acd5194a4f55089
sinbum Choi <sinbum@kakao.com>
2022-04-12 13:25:07 +0900

    일부수정

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-const {parsing} = require('./src/CrollIpo');
-const {writeJson,readJson,updateJson} = require('./src/FileManager');
+const { parsing } = require('./src/CrollIpo');
+const { writeJson, readJson, updateJson } = require('./src/FileManager');
 
 
 const directory = "database/ipo"
 const fileName = "4월"
 
 
-//이쪽에서 순서때문에 막힘.
-parsing()
-    .then((WebDatalist)=>{
-        //JSON.stringify(WebDatalist);
-        //console.log(WebDatalist); // 제이슨 포멧으로 돌려주는가?
-        //writeJson(directory,fileName,WebDatalist)    
-        
-        //readJson(directory,fileName);
-            //console.log('lcoalDataList',WebDatalist);        
-        updateJson(directory,fileName,WebDatalist);
 
-        return 
-        
+parsing()
+    .then((WebDatalist) => {
+        //writeJson(directory,fileName,WebDatalist)    
+        //readJson(directory,fileName);
+        updateJson(directory, fileName, WebDatalist);
+
+        return
     })
-    
-    .catch(e=>{return console.log("app.js 에서 parsing() 중 에러발생",e)})
+    .catch(e => { 
+        return console.log("app.js 에서 parsing() 중 에러발생", e) 
+    })
