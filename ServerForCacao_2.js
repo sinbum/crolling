b71c157d897fe3b56ad0116a7a3b57c496c02515
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-04-15 18:30:01 +0900

    SendMessege.js 파일 33번줄 메시지보낼때 오류 커밋 하기

---
+++ ServerForCacao_2.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const axios = require('axios');
+const qs = require('qs');
+const session = require('express-session');
+
+const KakaoLogin = require('./src/kakaoApi/KakaoLogin');
+const KakaoLoginInfoJson = require("./private/KakaoLoginInfo.json");
+const kakaoLogin = new KakaoLogin(qs.parse(KakaoLoginInfoJson));
+const loginInfo = kakaoLogin.getLoginInfo();
+console.log('로그인정보', loginInfo)
+const authUrl = kakaoLogin.getKakaoAuthURL(loginInfo);
+
+
+const kakaoApiMessege = require('./src/kakaoApi/SendMessege');
+
+
+let token = {};
+
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+})
+
+//세션을 설정할 때 쿠키가 생성된다.&&req session의 값도 생성해준다. 어느 라우터든 req session값이 존재하게 된다.
+app.use(session({
+    secret: 'ras',
+    resave: true,
+    secure: false,
+    saveUninitialized: false,
+}))
+
+app.get('/auth/kakao', (req, res) => {
+
+    //res.send('success');
+    console.log(authUrl)
+    res.redirect(authUrl); //authurl
+})
+
+
+app.get('/auth/kakao/callback', async (req, res) => {
+
+    let requestInfo = kakaoLogin.getReqestInfo(loginInfo);
+    requestInfo.data.code = req.query.code;
+    requestInfo.data = qs.stringify(requestInfo.data);
+
+    console.log('req.query.code', req.query.code)
+    console.log(requestInfo);
+    //access토큰을 받기 위한 코드
+    try {
+        token = await axios(requestInfo)
+        //console.log('token', token)
+    } catch (err) {
+        //console.log('에러발생')
+        res.json(err.data);
+    }
+
+    //access토큰을 받아서 사용자 정보를 알기 위해 쓰는 코드
+    let user;
+    try {
+        // console.log('토큰정보', token);//access정보를 가지고 또 요청해야 정보를 가져올 수 있음.
+        user = await axios(kakaoLogin.getRequestUserData(token.data.access_token))
+    } catch (e) {
+        res.json(e.data);
+    }
+    //console.log(user);
+
+    req.session.kakao = user.data;
+    //res.send('success');
+    res.redirect('localhost:8080')
+});
+
+
+app.get('/auth/info', (req, res) => {
+    let {nickname, profile_image} = req.session.kakao.properties;
+
+    //console.log(req.session.kakao.properties);
+
+    res.render('info', {
+        nickname, profile_image,
+    })
+});
+
+app.get('/', (req, res) => {
+    //console.log('렌더시작');
+    res.render('main');
+});
+
+
+app.get(kakaoLogin.redirectUri)
+
+
+app.get('/kakao/sendMessege', async (req, res) => {
+    let status;
+
+    if (token) {
+        status = await kakaoApiMessege(token.data.access_token);
+        console.log('status', status)
+    }
+
+    if (status == 200) {
+        console.log(status)
+        res.json({message: "ok!"})
+        // res.status(200).end();
+    }
+    // if (status != 200) {
+    //     res.status(404).end();
+    // }
+
+
+});
+
+
+app.listen(3000, () => {
+    console.log(`server start 3000`);
+})
+
+app.listen(8080, () => {
+    console.log(`server start 8080`);
+})
+
+app.listen(433, () => {
+    console.log(`server start 433`);
+})
+
+
+
--- src/kakaoApi/KakaoLogin.js
+++ src/kakaoApi/KakaoLogin.js
@@ -1,3 +1,5 @@
+const qs = require("qs");
+
 class KakaoLogin {
 
     constructor(loginInfo) {
@@ -25,9 +27,58 @@ class KakaoLogin {
         }
     }
 
+    //kakaoLoginInfo 반환
     getLoginInfo() {
-        //console.log('이객체 반환', this)
         return this
+    }
+
+    getKakaoAuthURL(kakaoLoginInfo) {
+        if (!kakaoLoginInfo) {
+            console.log("getReqestInfo()", "로그인 엑세스 정보가 없음")
+        }
+        const scope = 'scope=profile_nickname,account_email,profile_image,talk_message';
+        const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoLoginInfo.clientID}&redirect_uri=${kakaoLoginInfo.redirectUri}&response_type=code&` + scope;
+        return kakaoAuthURL
+    }
+
+    getReqestInfo(kakaoLoginInfo) {
+
+        if (!kakaoLoginInfo) {
+            console.log("getReqestInfo()", "로그인 엑세스 정보가 없음")
+        }
+
+        return {//token
+            method: 'POST',
+            url: 'https://kauth.kakao.com/oauth/token',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+                grant_type: 'authorization_code',//특정 스트링
+                client_id: kakaoLoginInfo.clientID,
+                client_secret: kakaoLoginInfo.clientSecret,
+                redirectUri: kakaoLoginInfo.redirectUri,
+
+                //주의()!!!!!!
+                //code: req.query.code,//결과값을 반환했다. 안됐다.
+
+            }//객체를 string 으로 변환
+        }
+    }
+
+    getRequestUserData(access_token) {
+        if (!access_token) {
+            console.log('getRequestUserData() 에러발생', '액세스토큰 이없음.')
+            return
+        }
+
+        return {
+            method: 'get',
+            url: 'https://kapi.kakao.com/v2/user/me',
+            headers: {
+                Authorization: `Bearer ` + access_token
+            }//헤더에 내용을 보고 보내주겠다.
+        }
     }
 
 
@@ -35,4 +86,7 @@ class KakaoLogin {
 
 
 module.exports = KakaoLogin
+
+
+
 
